ce8f0af1f1b1a8c5716fed645787b714fbba9041
melihcanclk <melihcanclk@hotmail.com>
2020-08-05 18:21:43 +0300

    Covid-19 raw source updated

--- src/main/java/com/melihcanclk/coronavirustracker/services/CoronaVirusService.java
+++ src/main/java/com/melihcanclk/coronavirustracker/services/CoronaVirusService.java
@@ -18,7 +18,7 @@ import java.util.List;
 
 @Service
 public class CoronaVirusService {
-    private static String VIRUS_URL = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv";
+    private static String VIRUS_URL = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv";
     private List<LoadModel> allModels = new ArrayList<>();
     private int minus = -1;
 
